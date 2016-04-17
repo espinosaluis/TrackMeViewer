17ed2c4472eaa9ea4b1c32bc77401f2834b49210
Fabian Neundorf <CommodoreFabianus@gmx.de>
2016-04-17 17:52:12 +0000

    Use meters per second by default
    Instead of storing km/h in the values it now stores the speed in meters per
    second and converts them later to kilometers per hour.

--- index.php
+++ index.php
@@ -970,7 +970,6 @@ sa.com/central_eng.php\">Luis Espinosa</a></div>\n";
                 }
                 $avg_speed /= $count;
 
-                $avg_kph = $avg_speed * 3.6;
                 if ($tripname == "Any")
                 {
                 $tripnameText = $trip_any_text;
@@ -987,7 +986,6 @@ sa.com/central_eng.php\">Luis Espinosa</a></div>\n";
                 for ($rounds = 1; $rounds <= $count; $rounds++)
                 {
                     $row = $result[$rounds - 1];
-                    $kph     = $row['Speed'] * 3.6;
                     // escape the strings for JS
                     $row['ImageURL'] = escape_js_str($row['ImageURL']);
                     $row['Comments'] = escape_js_str($row['Comments']);
@@ -1011,7 +1009,7 @@ sa.com/central_eng.php\">Luis Espinosa</a></div>\n";
 
                     $formattedTS = escape_js_str(date($date_format, strtotime($row['DateOccurred'])));
 
-                    $html .= "        trip.appendMarker({latitude: $row[Latitude], longitude: $row[Longitude], timestamp: '$row[DateOccurred]', speed: $kph, avgSpeed: $avg_kph, altitude: $row[Altitude], comment: '$row[Comments]', photo: '$row[ImageURL]' $dataParameter, formattedTS: '$formattedTS'}, $parameter);\n";
+                    $html .= "        trip.appendMarker({latitude: $row[Latitude], longitude: $row[Longitude], timestamp: '$row[DateOccurred]', speed: $row[Speed], avgSpeed: $avg_speed, altitude: $row[Altitude], comment: '$row[Comments]', photo: '$row[ImageURL]' $dataParameter, formattedTS: '$formattedTS'}, $parameter);\n";
                 }
 
 		if(isset($_REQUEST[last_location])) //show last location is on
